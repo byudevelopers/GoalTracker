68ff53c465ad10c89ad3b5940014e1a9e3ad54f3
Simon-Cheek <simon.cheek@yahoo.com>
2025-03-13 20:19:30 -0600

    Add app.properties to gitignore

--- devclub/src/main/java/com/byu/devclub/server/GoalController.java
+++ devclub/src/main/java/com/byu/devclub/server/GoalController.java
@@ -1,9 +1,11 @@
 package com.byu.devclub.server;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class GoalController {
 
     @GetMapping("/health")
