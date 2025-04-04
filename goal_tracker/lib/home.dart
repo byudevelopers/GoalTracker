60a1a13ef55f2e46420ab71c318f375edaa2cb01
Spencer021 <63733047+MrCheif@users.noreply.github.com>
2025-04-03 19:56:02 -0600

    new create goal page

--- goal_tracker/lib/home.dart
+++ goal_tracker/lib/home.dart
@@ -26,7 +26,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Home',
           style: TextStyle(
             color: Colors.white,
